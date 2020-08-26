55019a64973400e1ed5d88dc6c748336ee057710
martijnjanssen <martijn0612+github@gmail.com>
2020-08-26 15:30:45 +0200

    Basic configuration for non-nix os

---
+++ home.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, ... }:
+
+{
+  targets.genericLinux.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+    };
+  };
+
+  home.packages = with pkgs; [
+    htop
+    mlocate
+    tree
+
+    emacs
+    vim
+
+    spotify
+    vlc
+    tdesktop
+
+    openttd
+  ];
+
+  home.file.".spacemacs".source = dotfiles/spacemacs/spacemacs;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "martijn";
+  home.homeDirectory = "/home/martijn";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "20.09";
+}
